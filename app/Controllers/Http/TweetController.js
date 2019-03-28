16b8c32d9b66cf673a24e462422750ee0f64f87f
Flávio <henriqueflavio55@gmail.com>
2019-03-28 20:30:28 -0300

    Update AdonisJS for TwitterReact

---
+++ app/Controllers/Http/TweetController.js
@@ -0,0 +1,113 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Tweet = use('App/Models/Tweet')
+const User = use('App/Models/User')
+
+/**
+ * Resourceful controller for interacting with tweets
+ */
+class TweetController {
+  /**
+   * Show a list of all tweets.
+   * GET tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({response}) {
+    return await Tweet.query().with('user').orderBy('updated_at', 'desc').fetch()
+  }
+  
+  /**
+   * Render a form to be used for creating a new tweet.
+   * GET tweets/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new tweet.
+   * POST tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, auth, response }) {
+    const data = request.only(['content'])
+    const user = await auth.getUser()
+  
+    const tweet = await Tweet.create({ user_id: user.id, ...data})
+
+    const post = Object.assign(tweet, { user });
+
+    return response.json(post)
+  }
+
+  /**
+   * Display a single tweet.
+   * GET tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params }) {
+    return await Tweet.query().where('id', params.id).with('user').fetch()
+  }
+
+  /**
+   * Render a form to update an existing tweet.
+   * GET tweets/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update tweet details.
+   * PUT or PATCH tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a tweet with id.
+   * DELETE tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, auth , response}) {
+    const tweet = await Tweet.findOrFail(params.id)
+
+    if(tweet.user_id != auth.user.id)
+      return response.status(401)
+    
+    return await tweet.delete()
+
+  }
+}
+
+module.exports = TweetController
---
+++ start/routes.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.post('/register', 'AuthController.register')
+
+Route.post('/authenticate', 'AuthController.authenticate')
+
+Route.get('/getUser', 'AuthController.getUser')
+
+Route.get('/home', 'AppController.index')
+
+Route.get('/', 'AppController.index')
+
+Route.group( () => {
+  Route.resource('tweets', 'TweetController')
+    .apiOnly()
+    .except('update')
+})
